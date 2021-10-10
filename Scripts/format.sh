d0b38177142b329d5bdbb44d2d21d4cc58433cf4
brapru <brapru@pm.me>
2021-10-10 01:12:15 -0400

    Scripts: Exclude the Build directory from clang-format

--- Scripts/format.sh
+++ Scripts/format.sh
@@ -2,13 +2,14 @@
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BASE_DIR="$( dirname "$SCRIPT_DIR" )"
+BUILD_DIR="./Build"
 
 format() {
     echo "Running clang-format on: $(basename $1)"
     clang-format -i $1
 }
 
-FILES=$(find $BASE_DIR -iname *.h -o -iname *.cpp)
+FILES=$(find $BASE_DIR -iname *.h -o -iname *.cpp -path $BUILD_DIR -prune)
 for FILE in $FILES
 do
     format "$FILE"
