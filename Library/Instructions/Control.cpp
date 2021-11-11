86c64eb1a8fe1790ad7644df4de3024a192f270a
brapru <brapru@pm.me>
2021-11-11 15:53:16 -0500

    Instructions: Return cycles for Control instructions

--- Library/Instructions/Control.cpp
+++ Library/Instructions/Control.cpp
@@ -1,38 +1,49 @@
 #include <CPU.h>
 
-void CPU::instruction_nop(void)
+unsigned long CPU::instruction_nop(void)
 {
+    return 4;
 }
 
-void CPU::instruction_di(void)
+unsigned long CPU::instruction_di(void)
 {
     m_interrupt_master_enable = false;
+
+    return 4;
 }
 
-void CPU::instruction_ei(void)
+unsigned long CPU::instruction_ei(void)
 {
     m_interrupt_master_enable = true;
+    
+    return 4;
 }
 
-void CPU::instruction_cpl(void)
+unsigned long CPU::instruction_cpl(void)
 {
     auto value = ~m_a.value();
     m_a.set(value);
 
     m_f.set_subtraction_flag(true);
     m_f.set_half_carry_flag(true);
+
+    return 4;
 }
 
-void CPU::instruction_scf(void)
+unsigned long CPU::instruction_scf(void)
 {
     m_f.set_subtraction_flag(false);
     m_f.set_half_carry_flag(false);
     m_f.set_flag_carry(true);
+
+    return 4;
 }
 
-void CPU::instruction_ccf(void)
+unsigned long CPU::instruction_ccf(void)
 {
     m_f.set_subtraction_flag(false);
     m_f.set_half_carry_flag(false);
     m_f.set_flag_carry(!m_f.flag_carry());
+
+    return 4;
 }
