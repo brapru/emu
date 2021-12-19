90883e28753229c611aaf14c4e14755693e931f0
brapru <brapru@pm.me>
2021-12-19 10:23:55 -0500

    Utils/File: Do not exit if rom file does not exist

--- Library/Utils/File.h
+++ Library/Utils/File.h
@@ -7,11 +7,6 @@ inline std::vector<uint8_t> read_rom_into_vector(std::string const& filename)
 {
     std::ifstream rom_file(filename, std::ios::in | std::ios::binary);
 
-    if (!rom_file) {
-        outln("Could not load rom file: {}", filename);
-        exit(1);
-    }
-
     std::vector<uint8_t> data((std::istreambuf_iterator<char>(rom_file)), std::istreambuf_iterator<char>());
     outln("Loaded {} bytes from ROM file: {}", data.size(), filename);
 
